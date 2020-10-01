4ff71d8cbd28cc832d98100dc8a0d61d35a134cf
surpuredhanashri <surpuredhanashri@gmail.com>
2020-10-01 10:33:42 +0530

    DONE

---
+++ app.js
@@ -0,0 +1,17 @@
+const $burger = document.querySelector(`.burger`);
+
+const $navLinksContainer = document.querySelector(`.nav-links-container`);
+
+const $navLinks = document.querySelectorAll(`.nav-links-container li`);
+$burger.onclick = () => {
+    $navLinksContainer.classList.toggle(`nav-clicked`);
+    $burger.classList.toggle(`burger-clicked`);
+    $navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = ``;
+        }
+        else {
+            link.style.animation = `navLinksAnimation 0.5s ease forwards ${index / 4 + 0.5}s`;
+        }
+    });
+}
